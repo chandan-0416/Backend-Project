e30a357905bc32515f34537f6ab098b5d1888902
Chandan Kushwaha <chandankushwaha0411@gmail.com>
2025-11-26 00:43:38 +0530

    utils code discussion

---
+++ src/utils/ApiError.js
@@ -0,0 +1,27 @@
+// Api error file
+
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message= "Something went wrong",
+        errors = [],
+        stack = ""
+    ){
+        // overwrites
+      super(message)
+      this.statusCode = statusCode
+      this.data = null
+      this.message = message
+      this.success = false;
+      this.errors = errors
+
+      if (stack) {
+        this.stack = stack
+      } else{
+        Error.captureStackTrace(this, this.
+            constructor)
+      }
+    }
+}
+
+export {ApiError}
